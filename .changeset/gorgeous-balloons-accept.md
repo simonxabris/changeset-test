08d7d2b0885bf75b1813f94bc5f64051dbf909d5
Ábris Simon <abris96@gmail.com>
2022-12-04 21:06:02 +0100

    feat: change button

---
+++ .changeset/gorgeous-balloons-accept.md
@@ -0,0 +1,6 @@
+---
+"changeset-ui": minor
+"changeset-eslint-config-custom": patch
+---
+
+Modified button and eslint config
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop3</button>;
+  return <button>Boop4</button>;
 };
